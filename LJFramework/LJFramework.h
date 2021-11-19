217d63ab4bc2a0d8b51d0f588c0a8b2c05c2f456
崔志伟 <xiaowei5486@126.com>
2021-11-19 09:34:36 +0800

    Initial Commit

---
+++ LJFramework/LJFramework.h
@@ -0,0 +1,18 @@
+//
+//  LJFramework.h
+//  LJFramework
+//
+//  Created by 崔志伟 on 2021/11/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LJFramework.
+FOUNDATION_EXPORT double LJFrameworkVersionNumber;
+
+//! Project version string for LJFramework.
+FOUNDATION_EXPORT const unsigned char LJFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LJFramework/PublicHeader.h>
+
+
